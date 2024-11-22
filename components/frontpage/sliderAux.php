67ba6e465737fd5fdf89bc5efe9ff03b4032ac8e
keinnerross <keinnerross@gmail.com>
2024-11-22 20:38:07 -0300

    fixes

--- components/frontpage/sliderAux.php
+++ components/frontpage/sliderAux.php
@@ -10,7 +10,7 @@
 
 
 
-    <div class=" w-full min-h-[100px] md:min-h-[200px]  relative">    
+    <div class=" w-full min-h-[100px] md:min-h-[200px]  relative">
         <div class="w-full min-h-[100px] md:min-h-[200px]   bg-cover bg-center bg-no-repeat absolute z-[-1]" style="background-image: url(<?php echo $img1; ?>) " ;></div> <!-- close bg banner -->
         <div class="flex justify-center items-center h-full">
 
@@ -26,26 +26,28 @@
                 <div>
                     <div class="flex items-center justify-center  h-full  min-h-[100px] md:min-h-[200px] ">
                         <div class="flex flex-col gap-2 justify-center items-center ">
-                        <h2 class="md:text-4xl text-xs font-medium text-white">Priceless Traditions, Affordable Gifts</h2>
-                        <p class="md:text-base text-xs text-white">Meaningful gifts, prices you'll love.</p>
+                            <h2 class="md:text-4xl text-xs font-medium text-white">Priceless Traditions, Affordable Gifts</h2>
+                            <p class="md:text-base text-xs text-white">Meaningful gifts, prices you'll love.</p>
                         </div>
                     </div>
                 </div> <!-- close Item -->
                 <div>
                     <div class="flex items-center justify-center  h-full  min-h-[100px] md:min-h-[200px] ">
                         <div class="flex flex-col gap-2 justify-center items-center ">
-                        <h2 class="md:text-4xl text-xs font-medium text-white">Delivered to Your Door</h2>
-                        <p class="md:text-base text-xs text-white">Send a smile with a gift, delivered right to your door.</p>
+                            <h2 class="md:text-4xl text-xs font-medium text-white">Pick Up In-Store</h2>
+                            <p class="md:text-base text-xs text-white">Collect your order in-store or contact us for special arrangements. 
+                                
+                            </p>
                         </div>
                     </div>
                 </div> <!-- close Item -->
 
 
 
-                
 
 
-              
+
+
             </div>
 
         </div>
